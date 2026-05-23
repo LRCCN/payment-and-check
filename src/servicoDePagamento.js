852ea12cc8348d8edac9345888053bf2e52773fb
Luiz <luizitow@hotmail.com>
2026-05-23 14:24:44 -0400

    refactor: remove side-effect execution from payment service module

--- src/servicoDePagamento.js
+++ src/servicoDePagamento.js
@@ -19,8 +19,4 @@ class ServicoDePagamento {
   }
 }
 
-const servicoDePagamento = new ServicoDePagamento();
-servicoDePagamento.pagar('0987-7656-3475', 'Samar', 156.87);
-console.log(servicoDePagamento.consultarUltimoPagamento());
-
 export { ServicoDePagamento };
